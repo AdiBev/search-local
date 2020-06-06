66595c16af6ac603422171b200e98d62b0d20916
AdiBevoor <58564548+AdiBevoor@users.noreply.github.com>
2020-06-06 20:50:05 +0100

    Update file path for App.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 import { ApolloProvider } from "@apollo/react-hooks";
 import { client } from "./client";

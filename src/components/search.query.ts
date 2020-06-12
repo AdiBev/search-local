3249f4fc0a8e887180f63ddf85c7e7ab638f4ea7
AdiBevoor <58564548+AdiBevoor@users.noreply.github.com>
2020-06-12 21:10:28 +0100

    Add search query

--- src/components/search.query.ts
+++ src/components/search.query.ts
@@ -1,9 +1,39 @@
 import gql from "graphql-tag";
 
-export const SEARCH_PLACES = gql`
-  query SearchPlaces($term: String, $location: String) {
-    search(term: $term, location: $location) {
-      total
+export const SEARCH_DATA_FRAGMENT = gql`
+  fragment SearchData on Businesses {
+    business {
+      name
+      phone
+      id
+      price
+      coordinates {
+        latitude
+        longitude
+      }
+      location {
+        address1
+        city
+        postal_code
+        country
+      }
+      rating
+      review_count
+      url
+      photos
+      reviews {
+        rating
+        user {
+          name
+        }
+        text
+      }
     }
+  }
+`;
+
+export const SEARCH_TOTAL_FRAGMENT = gql`
+  fragment SearchTotal on Businesses {
+    total
   }
 `;

e2d6edaf667795c49e36a6b40e1c850b5805a4b0
AdiBevoor <58564548+AdiBevoor@users.noreply.github.com>
2020-06-12 21:09:29 +0100

    Add styled component Input

---
+++ src/components/shared/Input.tsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+import { inputFocusBorder, inputBorder, colorPrimary } from "../BaseStyles";
+import { motion } from "framer-motion";
+
+export const Input = styled(motion.input)`
+  height: 40px;
+  border-radius: 4px;
+  font-size: 16px;
+  color: ${colorPrimary};
+  padding-left: 1rem;
+  border: 1px solid ${inputBorder};
+
+  &:focus {
+    border: 1px solid ${inputFocusBorder};
+  }
+
+  &:disabled {
+    background: #3e486805 0% 0% no-repeat padding-box;
+    border: 1px solid #3e486826;
+  }
+`;

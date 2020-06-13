4d9c074fc5bbb9c14f9554623c7862fbfc33e595
AdiBevoor <58564548+AdiBevoor@users.noreply.github.com>
2020-06-13 21:15:42 +0100

    Add component to display review info

---
+++ src/components/reviews-list/ReviewInfoCard.tsx
@@ -0,0 +1,85 @@
+import React from "react";
+import styled from "styled-components";
+import user from "../../assets/user.svg";
+import {
+  HeaderTextSmall,
+  BodyText,
+  LinkText,
+} from "../themes/TypographyStyles";
+import { ReviewStars } from "./ReviewStars";
+import { colorPrimaryDark } from "../BaseStyles";
+import { Review } from "../../generated/graphql";
+
+const ReviewInfoCardWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 100px 1fr;
+  grid-column-gap: 20px;
+
+  .user-info-container {
+    img {
+      width: 45px;
+      height: 50px;
+      border-radius: 50%;
+    }
+    .user-name {
+      margin-top: 10px;
+    }
+  }
+
+  .info-container {
+    display: grid;
+    grid-template-rows: repeat(2, max-content);
+    grid-row-gap: 10px;
+
+    img {
+      height: 25px;
+      align-self: start;
+    }
+
+    .read-more {
+      &:hover {
+        cursor: pointer;
+        color: ${colorPrimaryDark};
+      }
+    }
+  }
+`;
+
+interface Props {
+  review: Review;
+}
+
+export const ReviewInfoCard: React.FunctionComponent<Props> = ({ review }) => {
+  const avatarUrl = review.user?.image_url ? review.user?.image_url : user;
+
+  const inCompleteReviewDisplayed =
+    review.text?.search("...") !== -1 ? true : false;
+
+  return (
+    <ReviewInfoCardWrapper>
+      <div className="user-info-container">
+        <img src={avatarUrl} alt="User avatar" />
+        <HeaderTextSmall className="user-name">
+          {review.user?.name!}
+        </HeaderTextSmall>
+      </div>
+      <div className="info-container">
+        <ReviewStars reviewRating={review.rating!} />
+        <BodyText>
+          {review.text!}
+          {inCompleteReviewDisplayed && (
+            <span className="read-more">
+              <LinkText
+                href={review.url!}
+                target="_blank"
+                rel="nopener nofollow"
+              >
+                Read more
+              </LinkText>
+            </span>
+          )}
+        </BodyText>
+      </div>
+    </ReviewInfoCardWrapper>
+  );
+};
